3d701ca1452ab161232552b75591966a47bffd8f
JavoxirMurodov <Javohir_9616@mail.ru>
2021-05-03 23:40:07 +0500

    Update numbers.py

--- Homework4/numbers.py
+++ Homework4/numbers.py
@@ -2,5 +2,5 @@ def ten():
     x = int(input())
     a = x % 10
     b = x // 10
-    print('The number of tens =', a)
-    print('The number of ones =', b)
+    print("The number of tens =", a)
+    print("The number of ones =", b)
